857dae373172beb9a46bd0c7ed738318cc31c9f3
iRuxu <17229398@qq.com>
2016-11-19 21:04:13 +0800

    wordpress

---
+++ wp-content/plugins/ucenter-integration/config.php
@@ -0,0 +1,16 @@
+<?php
+define('UC_CONNECT', 'mysql');
+define('UC_DBHOST', 'rds77viayyayymf.mysql.rds.aliyuncs.com');
+define('UC_DBUSER', 'jx3pve');
+define('UC_DBPW', '_thegirlisnoone#ow#PENTALKILL$');
+define('UC_DBNAME', 'jx3pve_bbs');
+define('UC_DBCHARSET', 'utf8');
+define('UC_DBTABLEPRE', '`jx3pve_bbs`.pre_ucenter_');
+define('UC_DBCONNECT', '0');
+define('UC_KEY', 'thegirlisnoone_#owQUEEN#PENTALKILL0512~!');
+define('UC_API', 'http://www.jx3pve.com/uc_server');
+define('UC_CHARSET', 'utf-8');
+define('UC_IP', '121.199.25.18');
+define('UC_APPID', '1');
+define('UC_PPP', '20');
+?>
