2d543335ac23e9dca086623366e94e205acf81a8
iRuxu <17229398@qq.com>
2016-11-19 20:35:08 +0800

    part.1 前端+down

---
+++ wp-content/themes/yylmacro-v2/footer.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains footer content and the closing of the #main and #page div elements.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
+ */
+?>
+<div class="wp-footer">
+	<iframe src="http://www.jx3pve.com/iframe/footer" height="209px" width="100%"></iframe>
+</div>
+</body>
+</html>
